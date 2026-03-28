1f298dc9a547d5589996f5e6cbb34a25a04145b7
XuZihao <xuzihao024572@gmail.com>
2026-03-28 20:32:56 +0800

    Add JUnit test for help and delete command

---
+++ src/test/java/seedu/finbro/commands/HelpCommandTest.java
@@ -0,0 +1,100 @@
+package seedu.finbro.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.finbro.exception.FinbroException;
+import seedu.finbro.ui.Ui;
+import seedu.finbro.utils.ExpenseList;
+
+public class HelpCommandTest {
+
+    @Test
+    public void execute_validCommandArgument_showsSpecificCommandHelp() throws FinbroException {
+        ExpenseList expenses = new ExpenseList();
+        TestUi ui = new TestUi();
+
+        HelpCommand command = new HelpCommand("add");
+        command.execute(expenses, ui, null);
+
+        assertEquals(0, ui.helpMessageCount);
+        assertEquals(1, ui.commandHelpMessageCount);
+        assertEquals(AddCommand.class, ui.lastCommand.getClass());
+        assertEquals(new AddCommand("").getHelpMessage(), ui.lastHelpMessage);
+    }
+
+    @Test
+    public void execute_validMultiWordMixedCaseArgument_showsSpecificCommandHelp() throws FinbroException {
+        ExpenseList expenses = new ExpenseList();
+        TestUi ui = new TestUi();
+
+        HelpCommand command = new HelpCommand("EDIT LIMIT");
+        command.execute(expenses, ui, null);
+
+        assertEquals(0, ui.helpMessageCount);
+        assertEquals(1, ui.commandHelpMessageCount);
+        assertEquals(EditCommand.class, ui.lastCommand.getClass());
+        assertEquals(new EditCommand("limit").getHelpMessage(), ui.lastHelpMessage);
+    }
+
+    @Test
+    public void execute_helpArgument_showsHelpCommandHelp() throws FinbroException {
+        ExpenseList expenses = new ExpenseList();
+        TestUi ui = new TestUi();
+
+        HelpCommand command = new HelpCommand("help");
+        command.execute(expenses, ui, null);
+
+        assertEquals(0, ui.helpMessageCount);
+        assertEquals(1, ui.commandHelpMessageCount);
+        assertEquals(HelpCommand.class, ui.lastCommand.getClass());
+        assertEquals(new HelpCommand("").getHelpMessage(), ui.lastHelpMessage);
+    }
+
+    @Test
+    public void execute_invalidCommandArgument_showsDefaultHelpMessage() throws FinbroException {
+        ExpenseList expenses = new ExpenseList();
+        TestUi ui = new TestUi();
+
+        HelpCommand command = new HelpCommand("unknown");
+        command.execute(expenses, ui, null);
+
+        assertEquals(1, ui.helpMessageCount);
+        assertEquals(0, ui.commandHelpMessageCount);
+        assertEquals(new HelpCommand("unknown").getHelpMessage(), ui.lastHelpMessage);
+    }
+
+    @Test
+    public void execute_blankArgument_showsDefaultHelpMessage() throws FinbroException {
+        ExpenseList expenses = new ExpenseList();
+        TestUi ui = new TestUi();
+
+        HelpCommand command = new HelpCommand("");
+        command.execute(expenses, ui, null);
+
+        assertEquals(1, ui.helpMessageCount);
+        assertEquals(0, ui.commandHelpMessageCount);
+        assertEquals(new HelpCommand("").getHelpMessage(), ui.lastHelpMessage);
+    }
+
+    private static class TestUi extends Ui {
+        private int helpMessageCount = 0;
+        private int commandHelpMessageCount = 0;
+        private String lastHelpMessage = "";
+        private Command lastCommand;
+
+        @Override
+        public void showHelpMessage(String message) {
+            helpMessageCount++;
+            lastHelpMessage = message;
+        }
+
+        @Override
+        public void showCommandHelpMessage(Command command) {
+            commandHelpMessageCount++;
+            lastCommand = command;
+            lastHelpMessage = command.getHelpMessage();
+        }
+    }
+}
